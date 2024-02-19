6ccf5b71e73f93a6f852678058886756ddf836af
Drahiri <kodzidg@o2.pl>
2024-02-19 18:06:14 +0100

    Block texture depends on isSolid

--- block.gd
+++ block.gd
@@ -3,6 +3,10 @@ extends StaticBody2D
 var isSolid := true
 @export var solid_block_texture := Texture2D
 
+func _ready():
+	if (isSolid):
+		$Sprite2D.texture = solid_block_texture
+
 func destroy():
 	if (not isSolid):
 		queue_free()

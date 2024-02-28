4c2147a7d445d436c37ca491a51b0b8765622914
Drahiri <kodzidg@o2.pl>
2024-02-28 10:02:16 +0100

    Ball script refactor

--- ball.gd
+++ ball.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 @export var initial_direction := Vector2(100.0, -350.0)
 @export var initial_speed: float = 364.0
 
-@onready var _direction: Vector2 = initial_direction.normalized()
 @onready var speed: float = initial_speed
+@onready var _after_stuck_velocity := initial_direction.normalized() * initial_speed
 
 var stuck := true
 var sticky := false
@@ -12,28 +12,32 @@ var active_sticky_count := 0
 var passthrough := false
 var active_passthrough_count := 0
 
+
 func _physics_process(delta: float):
 	if stuck:
+		velocity = Vector2()
+		move_and_slide()
 		if Input.is_action_just_pressed("release_ball"):
 			stuck = false
+			velocity = _after_stuck_velocity
 
-		move_and_slide()
 		return
 
-	var collider = move_and_collide(_direction * speed * delta)
-	if collider == null:
-		return
+	var collider = move_and_collide(velocity * delta)
+	if collider != null:
+		_resolve_collisions(collider.get_collider(), collider.get_normal())
 
-	var normal_collision = collider.get_normal()
-	collider = collider.get_collider()
 
+func _resolve_collisions(collider: Node2D, collision_normal: Vector2):
 	if collider.name == "Paddle":
-		_direction = (position - collider.position).normalized()
+		velocity = (position - collider.position).normalized() * speed
+		_after_stuck_velocity = velocity
 		stuck = sticky
-	else:
-		if collider.is_in_group("blocks") and not collider.is_solid:
-			collider.destroy()
-			if passthrough:
-				return
+		return
 
-		_direction = _direction.bounce(normal_collision)
+	if collider.is_in_group("blocks") and not collider.is_solid:
+		collider.destroy()
+		if passthrough:
+			return
+
+	velocity = velocity.bounce(collision_normal)

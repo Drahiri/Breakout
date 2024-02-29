2c76c16a71752d4635d517e0e7742e2aa0ed6bc8
Drahiri <kodzidg@o2.pl>
2024-02-29 17:39:11 +0100

    Add Chaos Effect

--- power_ups/power_up_chaos.gd
+++ power_ups/power_up_chaos.gd
@@ -1,9 +1,12 @@
 extends PowerUp
 
+@onready var chaos_effect = get_node("/root/Main/Effects/Chaos")
 
 func _effect():
 	$EffectDuration.start()
+	chaos_effect.show()
 
 
 func _on_effect_duration_timeout():
+	chaos_effect.hide()
 	queue_free()

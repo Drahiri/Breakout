f58016f21851ec0f3a3bab62be4dd45167d6a6c6
Drahiri <kodzidg@o2.pl>
2024-03-03 16:05:34 +0100

    Remove 'collected' flag

--- power_ups/power_up.gd
+++ power_ups/power_up.gd
@@ -4,8 +4,6 @@ class_name PowerUp
 @onready var Ball = get_node("/root/Main/Ball")
 @onready var Paddle = get_node("/root/Main/Paddle")
 
-var collected := false
-
 func _ready():
 	linear_velocity.y = 150.0
 
@@ -19,14 +17,13 @@ func _effect():
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	if not collected:
-		queue_free()
+	queue_free()
 
 
 func _on_body_entered(_body):
 	$Sprite2D.hide()
 	$CollisionShape2D.set_deferred("disabled", true)
-	collected = true
+	$VisibleOnScreenNotifier2D.free()
 	_effect()
 
 
